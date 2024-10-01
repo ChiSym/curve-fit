69bf117324a292a41ff0332f8e041542a6f04ea4
Colin Smith <colin.smith@gmail.com>
2024-10-01 13:22:53 -0700

    wip

--- notebooks/investigation.py
+++ notebooks/investigation.py
@@ -108,23 +108,23 @@ def update_coefficient(tr: genjax.Trace, *, scale=0.01, key):
     c_loc, c_scale = coef_d.args
     c0s = jnp.array([logpdf_normal(c, c_loc, c_scale) for c in c0])
     c1s = jnp.array([logpdf_normal(c, c_loc, c_scale) for c in c1])
-    print(f'delta: {delta}')
-    print(f'c0   : {c0}')
-    print(f'c1   : {c1}')
-    print(f'c0s  : {c0s} {jnp.sum(c0s)}')
-    print(f'c1s  : {c1s} {jnp.sum(c1s)}')
-    print(f'c1-0s: {c1s - c0s}')
-    print(f'diff : {jnp.sum(c0s) - jnp.sum(c1s)}')
+    # print(f'delta: {delta}')
+    # print(f'c0   : {c0}')
+    # print(f'c1   : {c1}')
+    # print(f'c0s  : {c0s} {jnp.sum(c0s)}')
+    # print(f'c1s  : {c1s} {jnp.sum(c1s)}')
+    # print(f'c1-0s: {c1s - c0s}')
+    # print(f'diff : {jnp.sum(c0s) - jnp.sum(c1s)}')
     choices = tr.get_choices()
     sigma_in = choices['sigma_inlier']
     outliers = choices['ys',...,'outlier']
-    print(f'outs : {outliers}')
+    # print(f'outs : {outliers}')
 
     # now do the ys
     def y_score(y, outlier, tr_y, new_y):
         return jax.lax.select(
             outlier,
-            0.0,
+            0.0, # the target ys don't change, and that's what this part of the score is based
             logpdf_normal(new_y, y, sigma_in) - logpdf_normal(tr_y, y, sigma_in)
         )
     fn = tr.subtraces[3].inner.get_args()[1]
@@ -132,98 +132,64 @@ def update_coefficient(tr: genjax.Trace, *, scale=0.01, key):
     new_fn = tr_u.subtraces[3].inner.get_args()[1]
     new_ys = new_fn(xs)[:, 0]
     y_scores = jax.vmap(y_score)(ys, outliers, tr_ys, new_ys)
-    print(f'y_difs: {y_scores}')
+    # print(f'y_difs: {y_scores}')
 
     #imp = importance_score(tr_u) - importance_score(tr)
     imp = jnp.sum(y_scores)
-    print(f'importance score {imp}')
+    # print(f'importance score {imp}')
 
     # I think this accounts for the difference in the 0th subtrace. There's a little gap to fill though.
     our_w  = jnp.sum(c1s - c0s) + imp
-    print(f'our_w: {our_w}')
+    # print(f'our_w: {our_w}')
     return tr_u, w_u, our_w
 
 def update_score(tr, cm):
     pass
 
-def test_update_coefficient(key):
+def test_update(key):
     k1, k2 = jax.random.split(key)
     #tr, w = importance_sample(cf, 1000, k1)
     cmap = C['ys', jnp.arange(4), 'y', 'value'].set(ys)
     tr, w = cf.gf.importance(k1, cmap, (xs,))
     #tr, w = importance_sample(cf, 1, k1)
-    print('importance sample complete')
-    print(f'trace[2] {tr.subtraces[2]}')
-    print(f'outliers {tr.get_choices()['ys', ..., 'outlier']}')
     tr_u, w_u, our_w = update_coefficient(tr, key=k2)
     return tr, tr_u, w_u, our_w
 # %%
-tr0, tr_u, w_u, our_w = test_update_coefficient(jax.random.PRNGKey(113))
+tr0, tr_u, w_u, our_w = test_update(jax.random.PRNGKey(113))
 w_u, our_w, w_u - our_w
 # %%
-ss = jnp.array([t.get_score() for t in tr0.subtraces]) - jnp.array([t.get_score() for t in tr_u.subtraces])
-w_u, jnp.sum(ss), w_u - jnp.sum(ss), tr0.get_score() - tr_u.get_score()
+def test_update_batch(k):
+    _, _, w_u, our_w = test_update(k)
+    return w_u - our_w
 
-
+jax.vmap(test_update_batch)(jax.random.split(jax.random.PRNGKey(314159) ,100))
 # %%
-tr0.subtraces[0].subtraces[0].inner.inner
+# Bug?
+k = jax.random.PRNGKey(271828)
+tr_unjitted, _ = cf.gf.importance(k, C.n(), (xs,))
+tr_jitted, _ = jax.jit(cf.gf.importance)(k, C.n(), (xs,))
 # %%
-tr_u.subtraces[0].subtraces[0].inner.inner
-# %%
-tr_u.subtraces[0].get_retval(), tr0.subtraces[0].get_retval()
-# %%
-logpdf_normal(1.0, 0.0, 2.0) - logpdf_normal(0.8124056, 0.0, 2.0)
-# %%
-tr0.subtraces[0].subtraces[0].get_retval()
-# %%
-sigma_inlier.get_args()
-# %%
-def h(t):
-    return sum(t.subtraces[0].subtraces[0].inner.inner.inner.subtraces[0].get_score())
-
-h(tr_u) - h(tr0), w_u
-# %%
-tr_u.subtraces[0].subtraces[0].inner.inner.inner.subtraces[0]
-# %%
-coef_d.args
-# %%
-tr0.get_choices()['ys',...,'outlier']
-# %%
-tr0.subtraces[0].get_retval()(xs), tr_u.subtraces[0].get_retval()(xs)
+tr_unjitted.subtraces[2], tr_jitted.subtraces[2]
 
 # %%
-ys
-# %%
-importance_score(tr0)
-# %%
-importance_score(tr_u)
-# %%
-logpdf_uniform(0.9999, -1.0, 1.0)
-# %%
-# let's take a step back. is the weight the difference of the scores?
-tr_u.get_score() - tr0.get_score(), w_u
-# %%
-# No!
 @genjax.gen
-def model():
-    return genjax.beta(1.0,1.0) @ 'x'
+def repro_model():
+    return genjax.beta(1.0, 1.0) @ 'x'
 
-trm_0 = model.simulate(jax.random.PRNGKey(1), ())
+k = jax.random.PRNGKey(271828)
+m_unjitted, _ = repro_model.importance(k, C.n(), ())
+m_jitted, _ = jax.jit(repro_model.importance)(k, C.n(), ())
+m_unjitted.get_score(), m_jitted.get_score()
+
 # %%
-trm_1, w, _, _ = trm_0.update(jax.random.PRNGKey(1), C.n())
+m_jitted
 # %%
-trm_2 = model.importance(jax.random.PRNGKey(2), C.n(), ())
+genjax.beta.logpdf(0.6, 1.0, 1.0), jax.jit(genjax.beta.logpdf)(0.6, 1.0, 1.0)
+
 # %%
-jax.jit(model.importance)(jax.random.PRNGKey(2), C.n(), ())
-trm_0
+s_unjitted = repro_model.simulate(k, ())
+s_jitted = jax.jit(repro_model.simulate)(k, ())
 # %%
-tr0.subtraces[2]
-# %%
-genjax.beta.logpdf(0.99, 1.0, 1.0)
-# %%
-genjax.beta.logpdf(0.5692411, 1.0, 1.0)
-# %%
-trm_2
-# %%
-tr0.subtraces[2]
+s_unjitted.get_score(), s_jitted.get_score()
+
 # %%
